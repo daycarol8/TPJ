172e3056050c912e4b363c6c08cf7d035cc29d9a
Dayanne <daycarol848@gmail.com>
2019-10-03 09:53:12 -0300

    Primeira e Segunda ativades da 4 unidade implementadas

---
+++ Assets/3bimestre/Fase2/movimentacao.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class movimentacao : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D bola;
+    float move;
+    [SerializeField] private float speed;
+    [SerializeField] private float forcapulo;
+    [SerializeField] private Text pontuacao;
+    [SerializeField] private Canvas canvas;
+    private int pont;
+    
+    void Start()
+    {
+        //forcapulo = 1000;
+        //pont = 0;
+        pont = PlayerPrefs.GetInt("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            bola.AddForce(Vector2.up * forcapulo);
+            
+        }
+        pontuacao.text = "Pontuação: " + pont;
+        PlayerPrefs.SetInt("Score", pont);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        move = Input.GetAxis("Horizontal");
+        bola.velocity = new Vector2(move * speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("pont"))
+        {
+            pont++;
+            Destroy(collision.gameObject);
+        }
+        else if(collision.CompareTag("Portal"))
+        {
+            SceneManager.LoadScene("4At1");
+            DontDestroyOnLoad(this.gameObject);
+            DontDestroyOnLoad(canvas);
+        }
+    }
+
+}
