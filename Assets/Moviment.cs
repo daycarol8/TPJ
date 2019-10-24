05f40576c86878db4fa75b803ac47ff746ab5be3
Dayanne <daycarol848@gmail.com>
2019-10-24 09:14:46 -0300

    atividade de animação

---
+++ Assets/Moviment.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public Animator anim;
+    private bool idle;
+    private bool andar;
+    public SpriteRenderer sprite;
+
+    private void Update()
+    {
+        anim.SetBool("idle", idle);
+        anim.SetBool("andar", andar);
+
+        Move();
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            idle = false;
+            andar = true;
+            sprite.flipX = false;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            idle = false;
+            andar = true;
+            sprite.flipX = true;
+        }
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            idle = true;
+            andar = false;
+            
+        }
+    }
+    void Move()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+    }
+
+
+}
